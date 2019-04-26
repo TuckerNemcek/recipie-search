419029af5dfec80d9c40dfbc2890ecc076dbdf36
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-26 11:18:26 -0600

    form component is hooked up correctly

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Form from './components/Form'
 
 class App extends Component {
+  getRecipie = (e) => {
+    e.preventDefault()
+    console.log("working!!!")
+  }
   render(){
     return (
       <div className ="App">
       <header className="App-header">
       <h1 className= "App-title">Recipie Search</h1>
       </header>
-      <Form />
+      <Form getRecipie = {this.getRecipie}/>
       </div>
     )
   }
