06c39bd886e346fb3e7768865d2498019fadf0d6
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-27 14:40:20 -0600

    damn that is a good lookin app

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 const Form = props => (
-  <form onSubmit = {props.getRecipie}>
-    <input type="text" name="recipieName"/>
-    <button>Search</button>
+  <form onSubmit = {props.getRecipie} style ={{ marginBottom:"2rem"}}>
+    <input className = "form__input" type="text" name="recipieName"/>
+    <button className = "form__button">Search</button>
   </form>
 )
 
