8504e15f791450981ea4f2f011f640b402c2cecf
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-29 12:59:40 -0600

    I continue to get an error stating I cannot map over null

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
+
 import React, { Component } from 'react';
 import './App.css';
 
-import Form from './components/Form'
-import Recipes from './components/Recipes'
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
-const API_KEY = "c63dc9393d58622ca72b8f88fa0d8f96"
-
+const API_KEY = "c63dc9393d58622ca72b8f88fa0d8f96";
 
 class App extends Component {
   state = {
     recipes: []
   }
-  getRecipie = async (e) => {
-    const recipeName = e.target.elements.recipieName.value
-    e.preventDefault()
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`)
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
 
-    const data = await api_call.json()
-    this.setState({recipes: data.recipes})
-    console.log(this.state.recipes)
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   }
-  render(){
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+  render() {
     return (
-      <div className ="App">
-      <header className="App-header">
-      <h1 className= "App-title">Recipie Search</h1>
-      </header>
-      <Form getRecipie = {this.getRecipie}/>
-      <Recipes recipes = {this.state.recipes} />
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-
-import App from "../App"
-import Recipe from "./Recipe"
+import App from "../App";
+import Recipe from "./Recipe";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App}/>
-      <Route path="/recipe" component={Recipe}/>
+      <Route path="/" component={App} exact />
+      <Route path="/recipe/:id" component={Recipe} />
     </Switch>
   </BrowserRouter>
-)
+);
 
-export default Router
+export default Router;
